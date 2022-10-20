ff4c2262758a1d540e0c054ca499e6c9c7f76790
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-10-21 00:48:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { Client, GatewayIntentBits, EmbedBuilder } from 'discord.js';
+import cron from 'node-cron';
 
 const client = new Client({
   intents: [
@@ -9,18 +10,19 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
   ],
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
 client.on('guildMemberAdd', async (member) => {
-  // console.log(member.guild.roles.guild.roles);
-  const role = await member.guild.roles.cache.get('1030058327121604639');
+  // auto-assign a role
+  const role = await member.guild.roles.cache.get(process.env.ROLE_ID);
   await member.roles.add(role);
 
+  // send a welcome message with an embed on the system default channel
   const channelID = member.guild.systemChannelId;
-  // console.log(channelID);
   const channel = await client.channels.fetch(channelID);
   const welcomeEmbed = new EmbedBuilder();
 
@@ -38,7 +40,24 @@ client.on('guildMemberAdd', async (member) => {
     embeds: [welcomeEmbed],
   });
 
+  // send a DM to the newly joined member
   member.send(
     "Hello, Welcome to Inheritance '22. We hope you have a great time here!"
+  );
+});
+
+/*  00 -> Minute
+    12 -> Hour
+    * -> Day of month (Any is fine)
+    * -> Month (Any is fine)
+    7 -> Day of week (Sunday)
+*/
+cron.schedule('00 12 * * 7', function () {
+  // remind mentees to update their mentors on 12pm of every Sunday
+  const channel = client.channels.cache.get(
+    process.env.CHANNEL_ID_ANNOUNCEMENTS
+  );
+  channel.send(
+    `Update time <@&${process.env.ROLE_ID_MENTEES}>! Provide your weekly updates to your respective mentors.`
   );
 });
